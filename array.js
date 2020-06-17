9014b591c89bca00697123f2b16a301a10d1d60b
paulohenriquec <paul.cavalcantesilva@gmail.com>
2020-06-17 18:49:01 -0300

    added vetores

---
+++ array.js
@@ -0,0 +1,16 @@
+const programador = {  //aqui temos um objeto
+    nome: "Paulo",
+    idade: 21,
+    tecnologias: [  //uma das propriedades desse objeto é um array, que é uma coleção de itens
+        {  //aqui temos um item do array tecnologias e esse item pode ser acessado através do indice 0 e esse item é um objeto com mais propriedades
+            nome: "Javascript",
+            especialidade: "Web"
+        },   //separação do outro item através da vírgula
+        {   //item de indice 1
+            nome:"C",
+            especialidade: "Desktop"
+        }
+    ] //fim do array
+} // fim do objeto programador
+
+console.log(`O programador ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade}`)
