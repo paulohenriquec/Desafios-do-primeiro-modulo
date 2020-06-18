86b59ac7af2677ed4f1c8c2ca6b6e0861b23f0e9
paulohenriquec <paul.cavalcantesilva@gmail.com>
2020-06-17 21:15:55 -0300

    added challenge Busca por tecnologia

---
+++ search-for-techs.js
@@ -0,0 +1,45 @@
+const usuarios = [
+    {
+        nome: "Paulo",
+        tecnologias: [ // array de strings
+            "HTML",
+            "CSS"
+        ]
+    },
+    {
+        nome: "Thamires",
+        tecnologias: [
+            "JavaScript", 
+            "CSS"
+        ]
+    },
+    {
+        nome: "Julia",
+        tecnologias: [
+            "HTML",
+            "Node.js"
+        ]
+    }
+]
+
+function checaSeUsuariousaCSS(usuario){
+    const techs = usuario.tecnologias;
+    for(let i=0;i<techs.length;i++){
+        if(techs[i]=="CSS")
+        {
+           return true;
+        }
+    }
+}
+
+function percorrerUsuarios(usuarios){ //ESSA FUNÇÃO CHAMA A FUNÇÃO DE CIMA
+    for(let i=0;i<usuarios.length;i++){
+        const checagem = checaSeUsuariousaCSS(usuarios[i]); // NESSE PONTO
+
+        if(checagem){ //AQUI VERIFICA SE CHECAGEM É TRUE
+            console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+        }
+    }
+}
+
+percorrerUsuarios(usuarios);
